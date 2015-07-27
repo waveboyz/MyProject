205cd3224bc7ff2080ae6a0b7e1becbd617ee6b4
waveboyz <waveboyz@wiiydeMacBook-Pro.local>
2015-07-27 09:42:08 +0800

    Initial Commit

---
+++ HYQ2.0/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HYQ2.0
+//
+//  Created by waveboyz on 15/7/27.
+//  Copyright (c) 2015年 HZHaoYuanQu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HYQ2.0/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HYQ2.0
+//
+//  Created by waveboyz on 15/7/27.
+//  Copyright (c) 2015年 HZHaoYuanQu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
